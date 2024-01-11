8e2e3ff6116a6d81d3c687e8a0b0fae0714dfbe0
sickk <jaakmyr@gmail.com>
2024-01-11 05:43:23 -0800

    OMG THIS JS CODE ICANT i need to use vars for a lot of stuff - im thinking of creating the whole window object thru js... but i mean i would have to create all the file folders ad such through js too which would suck. problem is me setting some vars in js and some not. i think i have to cahnge what js funcs im using and i could salvage. anywho i have to use a minvar var and im reusing the same lines over and over (i cant turn them into vars hahaahaahaahaah) i think im gouing to take a break on this and work on some other implementation then come back and hope i remember what in the world i was doing

--- js/draggableWindow.js
+++ js/draggableWindow.js
@@ -10,6 +10,7 @@ let windowStartWidth = 250; //temp vars for setting widht and height on start
 let windowStartHeight = 250;
 
 let onClickHeight;
+let onClickWidth;
 
 
 let norths = document.querySelectorAll('.northResize');
@@ -34,6 +35,7 @@ windows.forEach(function(dWindow){
 
         }
         onClickHeight = parseInt(dWindow.style.height);
+        onClickHeight = parseInt(dWindow.style.width);
         xRelativeClickStart = e.clientX - parseInt(dWindow.style.left);
         yRelativeClickStart = e.clientY - parseInt(dWindow.style.top);
     })
@@ -84,21 +86,38 @@ function resize(north, south, east, west, e){ //all bools
     if(north){
         let newHeight = (e.clientY - parseInt(currentWindow.style.top)); //neweight is the distance between the mouse and the top of the window
         newHeight = -(newHeight - parseInt(currentWindow.style.height)); //since we're going up the height will be negative (so we reverse taht) then we subtract (from the negative height (so it adds)) the height of the window on first click. before i had - 50 for min height but now it will adjust the window based on its height
-        currentWindow.style.height = newHeight + 'px';
 
-        currentWindow.style.top = e.clientY + 'px';
+        if(newHeight > 50){//I NEED MIN WIDTH VAR AAAAAA I SUCK AT CODING :sob:
+            currentWindow.style.height = newHeight + 'px';
+            currentWindow.style.top = e.clientY + 'px';
+        }else{
+            currentWindow.style.top = (parseInt(currentWindow.style.top) + parseInt(currentWindow.style.height) - 50) + 'px'; //left + width - min
+            currentWindow.style.height = 50 + 'px'; //love the magic numbers
+        } 
     }
     if(south){
-        currentWindow.style.height = (e.clientY - parseInt(currentWindow.style.top)) + 'px';
+        let newHeight = e.clientY - parseInt(currentWindow.style.top);
+        if(newHeight < 50){
+            newHeight = 50; //i feel like UGHGUOSKJFHSDLFS: i dont like this code it - it hurts me for some reason
+        }
+        currentWindow.style.height = newHeight + 'px';
     }
     if(east){
-        currentWindow.style.width = (e.clientX - parseInt(currentWindow.style.left)) + 'px'; //redo since its same as xclickstart
+        let newWidth = (e.clientX - parseInt(currentWindow.style.left)); //redo since its same as xclickstart
+        if(newWidth < 50)
+            newWidth = 50;
+        currentWindow.style.width = newWidth + 'px';
     }
     if(west){
         let newWidth = (e.clientX - parseInt(currentWindow.style.left)); //neweight is the distance between the mouse and the top of the window
         newWidth = -(newWidth - parseInt(currentWindow.style.width)); //since we're going up the height will be negative (so we reverse taht) then we subtract (from the negative height (so it adds)) the height of the window on first click. before i had - 50 for min height but now it will adjust the window based on its height
-        currentWindow.style.width = newWidth + 'px';
 
-        currentWindow.style.left = e.clientX + 'px';
+        if(newWidth > 50){//I NEED MIN WIDTH VAR AAAAAA I SUCK AT CODING :sob:
+            currentWindow.style.width = newWidth + 'px';
+            currentWindow.style.left = e.clientX + 'px';
+        }else{
+            currentWindow.style.left = (parseInt(currentWindow.style.left) + parseInt(currentWindow.style.width) - 50) + 'px'; //left + width - min
+            currentWindow.style.width = 50 + 'px'; //love the magic numbers
+        } //this is tricky bc i cant just set the width to 50 i need to move the left back to 50 away from the right side AAAs
     }
 }
