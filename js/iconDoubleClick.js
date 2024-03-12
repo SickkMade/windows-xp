d0aba792a968e006949277a80a741473f95d99f0
Jack Meyer <jaakmyr@gmail.com>
2024-03-11 18:19:31 -0700

    fix height syntax error

--- js/iconDoubleClick.js
+++ js/iconDoubleClick.js
@@ -7,7 +7,7 @@ icons.forEach(function(icon){
             dblClickWindow.style.top = 200 + 'px';
             dblClickWindow.style.left = 500 + 'px'; //NEED TO CREATE VAR FOR THIS!!! AND VARIATIONS
             dblClickWindow.style.width = 300 + 'px';
-            dblClickWindow.style.helight = 200 + 'px';
+            dblClickWindow.style.height = 200 + 'px';
 
             dblClickWindow.classList.remove('invisible'); //make it so you can see it
         }
